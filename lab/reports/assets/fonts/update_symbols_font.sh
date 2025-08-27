24c6e485e16fc27e31cc86b28cfa46ff719c4623
Martín Pozo <mpozo@protonmail.com>
2025-08-27 17:20:43 +0200

    fix(reports): close and undo icons

--- lab/reports/assets/fonts/update_symbols_font.sh
+++ lab/reports/assets/fonts/update_symbols_font.sh
@@ -1,7 +1,7 @@
 #!bash
 
 SYMBOLS=(.nf-fae-tools .nf-fa-sort_asc .nf-fa-sort_desc .nf-fa-filter .nf-oct-eye
-    .nf-fa-plus_circle .nf-fa-minus_circle .nf-md-close_circle .nf-md-backup_restore)
+    .nf-fa-plus_circle .nf-fa-minus_circle .nf-fa-close .nf-fa-undo)
 
 source=NerdFontSymbolsComplete
 sourcecss=$source.css
